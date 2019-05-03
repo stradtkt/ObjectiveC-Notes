bf2c96e09aaf7a5af41bbf4fd7810a7db649164e
Kevin <stradtkt22@gmail.com>
2019-05-03 06:24:50 -0400

    Added operators

--- operators/main.c
+++ operators/main.c
@@ -4,9 +4,6 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello world");
     }
-
-    bool hasLisence;
-    hasLisence = FALSE;
-    NSLog(@"has license: %i", hasLisence);
+    
     return 0;
 }
