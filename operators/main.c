d1a5bef10099ec37ce8ca74ac814a9dd362022dd
Kevin <stradtkt22@gmail.com>
2019-05-03 06:48:59 -0400

    Added operators

--- operators/main.c
+++ operators/main.c
@@ -4,6 +4,23 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSLog(@"Hello world");
     }
+
+    int apples = 3 + 5;
+
+    int oranges = 10 - 3;
+    int totalFruit = apples + oranges;
+
+    int eggsPerCarton = 12;
+    int eggs = eggsPerCarton * 4;
+
+    int baskets = 4;
+    float itemsPerBasket = (apples + oranges + eggs) / baskets;
+
+
+    bool compareResult;
+    compareResult = 5 > 3;
+
+    bool compareResult2 = 5 >= 5;
     
     return 0;
 }
